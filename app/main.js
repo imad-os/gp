dedaaf1d3f03b103e27489a3e0d9ed125b8a2cf4
Imad kh <imad@gmpanel.net>
2026-05-12 19:01:41 +0100

    Release v2026.05.12.6: bug fixes ...

--- app/main.js
+++ app/main.js
@@ -193,7 +193,7 @@ import { FirestoreRepository } from './modules/repository.js';
     const ALPHATAB_LOCAL_SOUNDFONT = '/assets/vendor/alphatab/package/dist/soundfont/sonivox.sf3';
     const APP_VERSIONS_URL = '/versions.json';
     const APP_BUILD = {
-      version: 'v2026.05.12.5',
+      version: 'v2026.05.12.6',
     };
     const LIBRARY_ADMIN_EMAILS = ['imad@gmail.com'];
     const LIBRARY_ADMIN_UIDS = [];
