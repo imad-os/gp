0efea6933e481f018a4e80a9a4fc5537e4acc686
Imad kh <imad@gmpanel.net>
2026-05-19 09:37:30 +0100

    Release v2026.05.19.7: bug fixes ...

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = '2026.05.19.6';
+const SW_VERSION = '2026.05.19.7';
 const STATIC_CACHE = `guitartrainer-static-${SW_VERSION}`;
 const RUNTIME_CACHE = `guitartrainer-runtime-${SW_VERSION}`;
 const CORE_ASSETS = [
